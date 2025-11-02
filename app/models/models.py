acf5cea915a275f3242b6a9ea91ad83e1fc22009
sveta060175-jpg <sveta060175@gmail.com>
2025-11-02 14:35:25 +0500

    Создали классы,модели

---
+++ app/models/models.py
@@ -0,0 +1,47 @@
+from typing import Optional
+from datetime import date,datetime
+from sqlmodel import SQLModel,Field,Relationship
+from sqlalchemy import func
+
+class User(SQLModel,table=True):
+    id: Optional[int]=Field(default=None,primary_key=True),
+    username: str=Field(index=True,unique=True),
+    fullname: Optional[str]=None,
+    email: Optional[str]=Field(default=None,index=True),
+    hashed_password: str
+    role: str="user" #Пользователи имеют роли admin,user
+    is_active: bool=True
+    created_at: datetime=Field(default_factory=func.now())
+
+class Privilege(SQLModel,table=True):
+    id: Optional[int]=Field(default=None,primary_key=True)
+    code: str=Field(unique=True,index=True)
+    name: str
+    description: Optional[str]=None,
+    created_at: datetime=Field(default_factory=func.now())
+
+class Citizen(SQLModel,table=True):
+    id: Optional[int]=Field(default=None,primary_key=True)
+    lastname: str
+    firstname: str
+    middlename: Optional[str]=None
+    id_snils: Optional[str]=Field(default=None,unique=True)
+    privilege_id: Optional[int]=Field(default=None,foreign_key="privilege.id")
+    created_at: datetime=Field(default_factory=func.now())
+
+class Certificates(SQLModel,table=True):
+    id: Optional[int]=Field(default=None,primary_key=True)
+    citizen_id: Optional[int]=Field(default=None,foreign_key="citizen.id")
+    number_certificate: Optional[str]=None
+    issue_date: Optional[date]=None
+    issued_by: Optional[str]=None
+    purpose: Optional[str]=None
+    created_at: datetime=Field(default_factory=func.now())
+
+class EventLog(SQLModel,table=True):
+    id: Optional[int]=Field(default=None,primary_key=True)
+    user_id: Optional[int]=Field(default=None,foreign_key="user.id")
+    action: str
+    object_type: Optional[str]=None
+    object_id: Optional[str]=None
+    created_at: datetime=Field(default_factory=func.now())
